6d2fca11f5269fc9257e8c4021485d4dbd271960
Fabián Gonzalo Artur de la Villarmois <gonzartur@gmail.com>
2021-08-26 21:27:25 +1200

    TASK-1: As a user I can't add a todo without a description

--- src/controllers.php
+++ src/controllers.php
@@ -73,6 +73,11 @@ $app->post('/todo/add', function (Request $request) use ($app) {
     $user_id = $user['id'];
     $description = $request->get('description');
 
+    if(!$description) {
+        $app['session']->getFlashBag()->add('validation', 'Description field is required');
+        return $app->redirect($request->headers->get('referer'));
+    }
+
     $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
     $app['db']->executeUpdate($sql);
 
